16e9fbde078e7a73ab1cd971fa6c12f9ca2e0700
nckvDev <jirapong.nckv@gmail.com>
2023-10-02 20:20:28 +0700

    fix: add column description shop

--- database/migrations/2023_08_13_060010_create_shops_table.php
+++ database/migrations/2023_08_13_060010_create_shops_table.php
@@ -14,6 +14,7 @@ return new class extends Migration
         Schema::create('shops', function (Blueprint $table) {
             $table->id();
             $table->string('shop_name');
+            $table->string('description');
             $table->string('status');
             $table->string('shop_image');
             $table->string('map_image');
